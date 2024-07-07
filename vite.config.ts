90dc2bba6dceb0738b047f80d827ef4c25174721
Lobooooooo14 <88998991+Lobooooooo14@users.noreply.github.com>
2024-07-07 11:45:05 -0300

    create base project

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src/"),
-      public: `${path.resolve(__dirname, "./public/")}`,
-      pages: path.resolve(__dirname, "./src/pages/"),
-      classes: `${path.resolve(__dirname, "./src/classes/")}`,
-      components: `${path.resolve(__dirname, "./src/components/")}`,
-      theme: `${path.resolve(__dirname, "./src/theme/")}`,
-      utils: `${path.resolve(__dirname, "./src/utils/")}`,
-      types: `${path.resolve(__dirname, "./src/@types/")}`
+      "@": path.resolve(__dirname, "./src/")
     }
   }
 })
