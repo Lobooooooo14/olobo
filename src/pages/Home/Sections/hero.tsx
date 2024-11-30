064087de0dd26631e7efa9f4c49098a14108a11e
Lobooooooo14 <88998991+Lobooooooo14@users.noreply.github.com>
2024-11-30 12:43:52 -0300

    fix: feat: improve responsivity in hero section

--- src/pages/Home/Sections/hero.tsx
+++ src/pages/Home/Sections/hero.tsx
@@ -18,10 +18,10 @@ export default function Hero() {
       />
 
       <div className="flex h-full w-full flex-col items-center justify-center">
-        <h1 className="text-4xl sm:text-6xl md:text-6xl lg:text-8xl">
+        <h1 className="text-center text-4xl sm:text-6xl md:text-6xl lg:text-8xl">
           {t("hero.title")}
         </h1>
-        <p className="m-0 text-nowrap bg-gradient-to-r from-rose-500 to-purple-400 bg-clip-text text-center font-mono text-lg font-bold uppercase tracking-widest text-transparent drop-shadow-[0_0_10px_rgba(244,64,96,0.8)] dark:from-cyan-400 dark:to-purple-400 dark:drop-shadow-[0_0_10px_rgba(121,168,246,0.8)] sm:text-xl md:text-2xl lg:text-3xl">
+        <p className="m-0 text-wrap bg-gradient-to-r from-rose-500 to-purple-400 bg-clip-text text-center font-mono text-lg font-bold uppercase tracking-widest text-transparent drop-shadow-[0_0_10px_rgba(244,64,96,0.8)] dark:from-cyan-400 dark:to-purple-400 dark:drop-shadow-[0_0_10px_rgba(121,168,246,0.8)] xs:text-nowrap sm:text-xl md:text-2xl lg:text-3xl">
           {t("hero.subtitle")}
         </p>
       </div>
